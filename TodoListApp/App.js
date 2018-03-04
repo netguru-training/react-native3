b8e5712e3906542e914fecce8aa76e22a2565a1e
stackninigi <stackninigi@gmail.com>
2018-03-04 11:55:20 +0100

    [add_tasks_list] updated with master

--- TodoListApp/App.js
+++ TodoListApp/App.js
@@ -1,23 +1,13 @@
-
-import React, { Component } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Platform,
-  Navigator,
-  StatusBar
-} from "react-native";
-import { StackNavigator } from "react-navigation";
-import { Provider } from "react-redux";
-import ListTaskContainer from "./components/ListTask/ListTaskContainer";
+import React, {Component} from "react";
+import {Platform, StyleSheet, Text, View} from "react-native";
+import {Provider} from "react-redux";
 import configureStore from "./redux/createStore";
-import DetailScreen from "./components/DetailScreen/DetailScreen";
 import {DATALOADING} from "./redux/Task/CheckBox/CheckBoxActions";
 import {loadState, saveState} from "./redux/localStorage";
 import throttle from "lodash/throttle";
+import Nav from "Navigation";
 
-class App extends React.Component {
+export default class App extends React.Component {
   constructor() {
     super();
 
@@ -46,48 +36,42 @@ class App extends React.Component {
       console.log('finished');
     });
   }
+  //
+  // getTaskList() {
+  //   const state = this.store.getState();
+  //   if (state.Task) {
+  //     return Object.values(state.Task);
+  //   }
+  //   return [];
+  // }
 
-  getTaskList() {
-    const state = this.store.getState();
-    if (state.Task) {
-      return Object.values(state.Task);
-    }
-    return [];
-  }
-
-  sampleTasks() {
-    return {
-      1: {
-        id: 1,
-        name: "Pierwszy task",
-        description: "Opis taska",
-        isDone: false
-      },
-
-      2: {
-        id: 2,
-        name: "Drugi lecz zrobiony",
-        description: "Task szybko wykonany",
-        isDone: true
-      }
-    };
-  }
+  // sampleTasks() {
+  //   return {
+  //     1: {
+  //       id: 1,
+  //       name: "Pierwszy task",
+  //       description: "Opis taska",
+  //       isDone: false
+  //     },
+  //
+  //     2: {
+  //       id: 2,
+  //       name: "Drugi lecz zrobiony",
+  //       description: "Task szybko wykonany",
+  //       isDone: true
+  //     }
+  //   };
+  // }
 
   render() {
     if(!this.state.storeReady) {
       return <Text>Spinner</Text>
     }
-    console.log('shoudl work', this.getTaskList());
     return (
       (
         <Provider store={this.store}>
           <View style={{ marginTop: Platform.select({ ios: 0, android: 20 }) }}>
-
-            {/*<FlatList style={styles.container}>*/}
-            {this.getTaskList().map(task => (
-              <ListTaskContainer key={task.id} id={task.id} />
-            ))}
-            {/*</FlatList>*/}
+            <Nav/>
           </View>
 
         </Provider>
@@ -95,13 +79,6 @@ class App extends React.Component {
     );
   }
 }
-
-export default StackNavigator({
-  Home: {
-    // screen: DetailScreen,
-    screen: App
-	},
-});
 
 const styles = StyleSheet.create({
   container: {
