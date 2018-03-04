f1214679bde613f23b07fc9aeabe53bd502efa84
stackninigi <stackninigi@gmail.com>
2018-03-04 10:48:21 +0100

    [003] button - rounded

--- TodoListApp/App.js
+++ TodoListApp/App.js
@@ -1,91 +1,83 @@
-
-import React, { Component } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Platform,
-  Navigator,
-  StatusBar
-} from "react-native";
-import { StackNavigator } from "react-navigation";
-import { Provider } from "react-redux";
+import React, {Component} from "react";
+import {Platform, StyleSheet, View} from "react-native";
+import {StackNavigator} from "react-navigation";
+import {Provider} from "react-redux";
 import ListTaskContainer from "./components/ListTask/ListTaskContainer";
 import configureStore from "./redux/createStore";
-import DetailScreen from "./components/DetailScreen/DetailScreen";
+import FloatingButton from "./components/FloatingButton/FloatingButton";
 
 class App extends React.Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      tasks: {},
-      store: null
-    };
+        this.state = {
+            tasks: {},
+            store: null
+        };
 
-    configureStore({
-      Task: this.sampleTasks()
-    }).then(store => {
-      this.setState({ store });
-      this.setState({ tasks: store.getState().Task });
-    });
-  }
-
-  getTaskList() {
-    // console.log('evaluating getTaskList', this.state.store.Task);
-    if (this.state.tasks) {
-      return Object.values(this.state.tasks);
+        configureStore({
+            Task: this.sampleTasks()
+        }).then(store => {
+            this.setState({store});
+            this.setState({tasks: store.getState().Task});
+        });
     }
-    return [];
-  }
 
-  sampleTasks() {
-    return {
-      1: {
-        id: 1,
-        name: "Pierwszy task",
-        description: "Opis taska",
-        isDone: false
-      },
+    getTaskList() {
+        if (this.state.tasks) {
+            return Object.values(this.state.tasks);
+        }
+        return [];
+    }
 
-      2: {
-        id: 2,
-        name: "Drugi lecz zrobiony",
-        description: "Task szybko wykonany",
-        isDone: true
-      }
-    };
-  }
+    sampleTasks() {
+        return {
+            1: {
+                id: 1,
+                name: "Pierwszy task",
+                description: "Opis taska",
+                isDone: false
+            },
 
-  render() {
-    return (
-      this.state.store && (
-        <Provider store={this.state.store}>
-          <View style={{ marginTop: Platform.select({ ios: 0, android: 20 }) }}>
-            {/*<FlatList style={styles.container}>*/}
-            {this.getTaskList().map(task => (
-              <ListTaskContainer key={task.id} task={task} />
-            ))}
-            {/*</FlatList>*/}
-          </View>
-        </Provider>
-      )
-    );
-  }
+            2: {
+                id: 2,
+                name: "Drugi lecz zrobiony",
+                description: "Task szybko wykonany",
+                isDone: true
+            }
+        };
+    }
+
+    render() {
+        return (
+            this.state.store && (
+                <Provider store={this.state.store}>
+                    <View style={{marginTop: Platform.select({ios: 0, android: 20})}}>
+                        {/*<FlatList style={styles.container}>*/}
+                        {this.getTaskList().map(task => (
+                            <ListTaskContainer key={task.id} task={task}/>
+                        ))}
+                        {/*</FlatList>*/}
+                        <FloatingButton/>
+                    </View>
+                </Provider>
+            )
+        );
+    }
 }
 
 export default StackNavigator({
-  Home: {
-    // screen: DetailScreen,
-    screen: App
-	},
+    Home: {
+        // screen: DetailScreen,
+        screen: App
+    },
 });
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
+    }
 });
---
+++ TodoListApp/components/FloatingButton/FloatingButton.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {Text, TouchableOpacity, View} from "react-native";
+
+const FloatingButton = () => {
+    onPress = () => {
+        console.log('sth')
+    };
+
+  return (
+    <View style={styles.container}>
+        <TouchableOpacity
+            style={styles.button}
+            onPress={this.onPress}
+        >
+            <Text> Touch Here </Text>
+        </TouchableOpacity>
+    </View>
+  )
+};
+
+FloatingButton.propTypes = {};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 10
+    },
+    button: {
+        width: 150,
+        height: 150,
+        borderRadius: 150/2,
+        backgroundColor: '#00BCD4'
+    }
+});
+
+export default FloatingButton;
